bae576de2f5dea7144964d355123c52b6930ddff
echendi <nidafa202426@gmail.com>
2024-04-01 12:24:08 -0500

    feat: Implementar bycript para proteger las contraseñas

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,15 @@ import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '../user/entities/user.entity';
 import { user } from '@prisma/client';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
     constructor(private readonly jwtService: JwtService, private readonly prisma: PrismaService) { }
 
     async validateUser(username: string, password: string): Promise<any> {
-        const user: User = await this.prisma.user.findFirst({ where: { username, password } });
-        if (user && user.password === password && user.username === username) {
+        const user: User = await this.prisma.user.findFirst({ where: { username } });
+        if (user && user.username === username && await bcrypt.compare(password, user.password)) {
             return user;
         }
         return null;
