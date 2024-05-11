7b0afa15cf034e5fbea215acfb34dccbd903031c
David Fajardo <nidafa202426@gmail.com>
2024-05-11 15:51:39 -0500

    feat: update is customer served

--- src/customer/dto/customerDTO/update-customer.input.ts
+++ src/customer/dto/customerDTO/update-customer.input.ts
@@ -1,50 +1,62 @@
-import { customer_tape_preference } from "@prisma/client";
-import { IsPositive, IsInt, IsNotEmpty, IsBoolean, Equals, IsEnum, IsString, IsOptional } from "class-validator";
+import { customer_tape_preference } from '@prisma/client';
+import {
+  IsPositive,
+  IsInt,
+  IsNotEmpty,
+  IsBoolean,
+  Equals,
+  IsEnum,
+  IsString,
+  IsOptional,
+} from 'class-validator';
 
 export class UpdateCustomerInput {
+  @IsPositive({ message: 'El id debe ser un numero positivo' })
+  @IsInt({ message: 'El id debe ser un numero entero' })
+  @IsNotEmpty({ message: 'El id no puede estar vacio' })
+  id: number;
 
-    @IsPositive({ message: "El id debe ser un numero positivo" })
-    @IsInt({ message: "El id debe ser un numero entero" })
-    @IsNotEmpty({ message: "El id no puede estar vacio" })
-    id: number
+  @IsString()
+  @IsOptional()
+  address?: string;
 
-    @IsString()
-    @IsOptional()
-    address?: string;
+  @IsString()
+  @IsOptional()
+  neighborhood?: string;
 
-    @IsString()
-    @IsOptional()
-    neighborhood?: string;
+  @IsInt({ message: 'El orden de la ruta debe ser un numero entero.' })
+  @IsOptional()
+  @IsPositive({ message: 'El orden de la ruta debe ser un valor positivo.' })
+  route_order?: number;
 
-    @IsInt({ message: 'El orden de la ruta debe ser un numero entero.' })
-    @IsOptional()
-    @IsPositive({ message: 'El orden de la ruta debe ser un valor positivo.' })
-    route_order?: number;
+  @IsOptional()
+  @IsEnum(customer_tape_preference)
+  tape_preference?: customer_tape_preference;
 
-    @IsOptional()
-    @IsEnum(customer_tape_preference)
-    tape_preference?: customer_tape_preference;
+  @IsOptional()
+  @IsBoolean({ message: 'El campo de contacto debe ser boolean' })
+  is_contactable?: boolean;
 
-    @IsOptional()
-    @IsBoolean({ message: 'El campo de contacto debe ser boolean' })
-    is_contactable?: boolean;
+  @IsOptional()
+  @IsBoolean({ message: 'El campo de atendido debe ser boolean' })
+  is_served?: boolean;
 
-    @IsString()
-    @IsOptional()
-    name?: string;
+  @IsString()
+  @IsOptional()
+  name?: string;
 
-    @IsString()
-    @IsOptional()
-    cellphone?: string;
+  @IsString()
+  @IsOptional()
+  cellphone?: string;
 
-    @IsOptional()
-    @IsInt({message:'El id de la ruta debe ser un numero entero'})
-    @IsPositive({message: 'El id de la ruta debe ser positivo'})
-    route_id?: number;
+  @IsOptional()
+  @IsInt({ message: 'El id de la ruta debe ser un numero entero' })
+  @IsPositive({ message: 'El id de la ruta debe ser positivo' })
+  route_id?: number;
 
-    @IsBoolean()
-    @Equals(true, { message: 'Se debe proporcionar un id' })
-    get isValid(): boolean {
-        return !!this.id;
-    }
+  @IsBoolean()
+  @Equals(true, { message: 'Se debe proporcionar un id' })
+  get isValid(): boolean {
+    return !!this.id;
+  }
 }
