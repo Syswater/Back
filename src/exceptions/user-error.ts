6d5925deafcd44fcf337a1d9bade4a2d6845a83e
David Fajardo <nidafa202426@gmail.com>
2024-05-11 14:08:02 -0500

    feat: change user password

--- src/exceptions/user-error.ts
+++ src/exceptions/user-error.ts
@@ -1,12 +1,13 @@
-import { SsError } from "./error-type";
+import { SsError } from './error-type';
 
 export class UserError extends SsError {
-    constructor(code: UserErrorCode, description?: any) {
-        super(code, UserErrorCode, UserError.name, description);
-    }
+  constructor(code: UserErrorCode, description?: any) {
+    super(code, UserErrorCode, UserError.name, description);
+  }
 }
 
 export enum UserErrorCode {
-    USER_NOT_FOUND = 'USER_NOT_FOUND',
-    EXISTING_USERNAME = 'EXISTING_USERNAME'
+  USER_NOT_FOUND = 'USER_NOT_FOUND',
+  EXISTING_USERNAME = 'EXISTING_USERNAME',
+  WRONG_USER_PASSWORD = 'WRONG_USER_PASSWORD',
 }
