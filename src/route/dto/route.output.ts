38ee58784f4f9e1c3806f9877e8baf0b48d06f05
David Fajardo <nidafa202426@gmail.com>
2024-05-17 23:52:15 -0500

    feat: param distribution_id on get routes

--- src/route/dto/route.output.ts
+++ src/route/dto/route.output.ts
@@ -1,11 +1,12 @@
-import { RouteStatus } from "../../constants/route-status";
-import { Weekday } from "../../constants/weekday";
+import { RouteStatus } from '../../constants/route-status';
+import { Weekday } from '../../constants/weekday';
 
 export class RouteDto {
-    id: number;
-    name: string;
-    location: string;
-    weekdays: Weekday[];
-    price: number;
-    status?: RouteStatus
+  id: number;
+  name: string;
+  location: string;
+  weekdays: Weekday[];
+  price: number;
+  status?: RouteStatus;
+  distribution_id?: number;
 }
