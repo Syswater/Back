71ecb1f8b8841578bcc0b285b35861851ae39ea3
David Fajardo <nidafa202426@gmail.com>
2024-05-18 11:55:44 -0500

    fix: distribution error

--- src/exceptions/distribution-error.ts
+++ src/exceptions/distribution-error.ts
@@ -1,12 +1,13 @@
-import { SsError } from "./error-type";
+import { SsError } from './error-type';
 
 export class DistributionError extends SsError {
-    constructor(code: DistributionErrorCode, description?: any) {
-        super(code, DistributionErrorCode, DistributionError.name, description);
-    }
+  constructor(code: DistributionErrorCode, description?: any) {
+    super(code, DistributionErrorCode, DistributionError.name, description);
+  }
 }
 
 export enum DistributionErrorCode {
-    STATUS_PREORDER_CHANGE = 'STATUS_PREORDER_CHANGE',
-    EXISTING_DISTRIBUTION = 'EXISTING_DISTRIBUTION'
+  STATUS_PREORDER_CHANGE = 'STATUS_PREORDER_CHANGE',
+  EXISTING_DISTRIBUTION = 'EXISTING_DISTRIBUTION',
+  USER_DISTRIBUTION_IS_ALREADY_REGISTERED = 'USER_DISTRIBUTION_IS_ALREADY_REGISTERED',
 }
